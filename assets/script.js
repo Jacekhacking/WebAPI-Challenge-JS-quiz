a79a2de1e4ec371fad2fb5ba14603bd23b26bacf
Jacekhacking <82614083+Jacekhacking@users.noreply.github.com>
2021-05-22 13:43:29 -0600

    added JS questions array and started making elementID's

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+//select elementsById
+var start = document.getElementById("start");
+var timer = document.getElementById("timer");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var correctAnswer = document.getElementById("correct");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+var filler = document.getElementById("");
+
+//create questions
+ var questions = [
+        {
+        question: "FIller question",
+        choiceA : "asdlf;kj",
+        choiceB : "asdfs",
+        choiceC : "asdfasg",
+        choiceD : "ahadfda",
+        correct :"C"
+        },{
+        question: "FIller question",
+        choiceA : "asdlf;kj",
+        choiceB : "asdfs",
+        choiceC : "asdfasg",
+        choiceD : "ahadfda",
+        correct :"C"
+        },{
+        question: "FIller question",
+        choiceA : "asdlf;kj",
+        choiceB : "asdfs",
+        choiceC : "asdfasg",
+        choiceD : "ahadfda",
+        correct :"C"
+        },{
+        question: "FIller question",
+        choiceA : "asdlf;kj",
+        choiceB : "asdfs",
+        choiceC : "asdfasg",
+        choiceD : "ahadfda",
+        correct :"C"
+        }
+];
+
+//timer stuff 
