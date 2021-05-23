e7de0d3ab972368cface7b204bf05ecbbb3bbf49
Jacekhacking <82614083+Jacekhacking@users.noreply.github.com>
2021-05-23 14:32:26 -0600

    more JS and CSS separate html pages created. createQuestions func added.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,19 @@
 //select elementsById
-var start = document.getElementById("start");
+var startBtn = document.getElementById("start");
 var timer = document.getElementById("timer");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
 var choiceD = document.getElementById("D");
 var correctAnswer = document.getElementById("correct");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-var filler = document.getElementById("");
-
+var playerInitial = document.querySelector('#initials');
+var highScores = document.querySelector('#highScores');
+var timerEl = document.getElementById("countdown");
+var mainEL = document.getElementById("main");
+var lastQuestion = questions.length -1;
+var runningQuetstions = 0
 //create questions
+
  var questions = [
         {
         question: "FIller question",
@@ -50,22 +45,52 @@ var filler = document.getElementById("");
         correct :"C"
         }
 ];
+
 //loop over questions object
 for( var i = 0; i < questions.length; i++){
-    var answer = "click"(questions[i].a);
+    var answer = correct(questions[i].a);
     
     if(answer == questions[q] .correct){
-        score++;
+        timer + 7;
         answerIsCorrect();
     }else {
-        score--;
+        timer -7;
         answerIsWrong();
     }
 
     
 }
 
-
+// function to create questions in html format 
+function createQuestions(){
+var q = questions[runningQUestions];
+question.innerHTML= "<p>"+ q.question + "<p>";
+choiceA.innerHTML = q.choiceA;
+choiceB.innerHTML = q.choiceB;
+choiceC.innerHTML = q.choiceC;
+choiceD.innerHTML = q.choiceD;
+}
 
 
 //timer stuff 
+function countdownTimer(){
+    var timeLeft = 60;
+    var timeInterval = setInterval(function(){
+        if (timeLeft>1){
+            timerEl.textContent =  "Time:" +  timeLeft;
+            timeLeft--;
+        }else if (timeLeft===1){
+            timerEl.textContent =  "Time:" +  timeLeft;
+            timeLeft--;
+        }else{
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            // make function that shows score asks for initials and submits initials and score to local storage.
+            insertFunctionHere
+        }
+    }, 1000)
+}
+
+
+
+startBtn.onClick = questions;
